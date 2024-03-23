6a78cc7e411ce6cfd830de8f634711a1a83e1631
drincon1 <d.rincon@uniandes.edu.co>
2024-03-23 13:08:52 -0500

    Home page design ready

--- src/app.py
+++ src/app.py
@@ -3,8 +3,20 @@ from dash import html, dcc, callback, Input, Output
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("Welcome to our Complex HTML Example")
+
+app.layout = html.Div(children=[
+    html.Div(className="background", children=[
+        html.Div(className="home-page", children=[
+            html.Div(className="title-buttons", children=[
+                html.H2(className="home-title", children="Autodiagnóstico: Dependencias e impactos"),
+                html.Div(className="home-buttons", children=[
+                    html.Button(className="btn-comenzar", children="Comenzar"),
+                    html.Button(className="btn-informacion", children="Información")
+                ])
+            ]),
+            html.Img(className="home-image", src="assets/home-image.jpg")
+        ])
+    ])
 ])
 
 if __name__ == '__main__':

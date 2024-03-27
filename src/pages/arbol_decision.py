0e304ce6e2e02db45b286f4165576ba3118b5b52
drincon1 <d.rincon@uniandes.edu.co>
2024-03-27 10:20:35 -0500

    Home Page y AD en formato dash html

--- src/pages/arbol_decision.py
+++ src/pages/arbol_decision.py
@@ -1,9 +1,68 @@
 import dash 
-from dash import html
+from dash import html, dcc
 
 dash.register_page(__name__)
 
-layout = html.Div([
-    html.H1('This is our Analytics page')
-])
-
+layout = html.Div(
+    className="background",
+    children=[
+        html.Div(
+            className="ad-layout",
+            children=[
+                html.Div(
+                    className="panel-control",
+                    children=[
+                        html.Label("Código CIIU"),
+                        dcc.Input(className="input-ciiu", id="ciiu", type="number", placeholder="####"),
+                        html.Label("Sector(es)"),
+                        dcc.Dropdown(className="select-sectores", options=['NYC', 'MTL', 'SF'], value='NYC', id='demo-dropdown'),
+                        html.Label("Sub-Sector(es)"),
+                        dcc.Dropdown(className="select-subsectores", options=['NYC', 'MTL', 'SF'], id='demo-dropdown'),
+                        html.Button(className="btn-buscar",children="Buscar"),
+                    ],
+                ),
+                html.Div(
+                    className="categorias",
+                    children=[
+                        html.Div(
+                            className="seleccion-categoria",
+                            children=[
+                                html.Button(className="btn-ctg", children="Ecosistema"),
+                                html.Button(className="btn-ctg", children="Servicios Eco"),
+                                html.Button(className="btn-ctg", children="Especies"),
+                            ],
+                        ),
+                        html.Div(
+                            className="elementos-categoria",
+                            children=[
+                                html.Div(
+                                    className="elemento",
+                                    children=[
+                                        html.H4(className="elemento-imagen", children="A"),
+                                        html.Div(
+                                            className="elemento-informacion",
+                                            children=[
+                                                html.Div(
+                                                    className="titulo-botones",
+                                                    children=[
+                                                        html.H3(className="elemento-titulo", children="Materias primas"),
+                                                        html.Button(html.Img(src="../assets/add.png", alt="+"), className="btn-add"),
+                                                        html.Button(html.Img(src="../assets/information.png", alt="i"), className="btn-info"),
+                                                    ],
+                                                ),
+                                                html.Div(
+                                                    className="elemento-descripcion",
+                                                    children="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et lorem ut lorem tincidunt tempus. Aenean eleifend ultricies leo sit amet efficitur.",
+                                                ),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ],
+)
--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,5 +1,6 @@
+import dash_bootstrap_components as dbc
 import dash 
-from dash import html, Output, Input, callback, dcc
+from dash import html, Output, Input, State, callback, dcc
 
 dash.register_page(__name__,path='/')
 
@@ -11,7 +12,7 @@ layout = html.Div(children=[
                 html.Div(className="home-buttons", children=[
                     html.Button(className="btn-comenzar", children="Comenzar", id='comenzar-button'),
                     dcc.Location(id='url', refresh=True),
-                    html.Button(className="btn-informacion", children="Información")
+                    # html.Button(className="btn-informacion", children="Información", id="open", n_clicks=0),
                 ])
             ]),
             html.Img(className="home-image", src="assets/home-image.jpg")
@@ -19,6 +20,7 @@ layout = html.Div(children=[
     ])
 ])
 
+# Callback para ir a la dirección URL /arbol-decision
 @callback(
     Output('url', 'pathname'),
     [Input('comenzar-button', 'n_clicks')]
